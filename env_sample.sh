449db8b096181c062519f105bf2e2dee7e6a38e3
niltea <futurabodoni@gmail.com>
2017-07-28 15:06:07 +0900

    first

---
+++ env_sample.sh
@@ -0,0 +1,16 @@
+export posts_limit=****
+
+export tumblr_consumer_key=****
+export tumblr_consumer_secret=****
+export token=****
+export token_secret=****
+
+export slack_webhook_URL=****
+export slack_icon_url=****
+export slack_username=****
+export slack_channel=****
+
+export aws_accessKeyId=****
+export aws_secretAccessKey=****
+export aws_region=****
+export aws_s3_saveBucket=****
---
+++ readme.md
@@ -0,0 +1,10 @@
+TumblrのLikesを自動で取得して画像を保存するやつ
+
+## 必要な環境
+AWS Lambda + Dynamo DBでの動作を念頭に作成しています
+
+## 設定
+環境変数に各種クレデンシャル等を入れる。
+詳細はenv_sample.shを参照のこと。
+
+Tumblr APIのtoken, token_secret は[こちら](https://api.tumblr.com/console/calls/user/info)より取得可。
